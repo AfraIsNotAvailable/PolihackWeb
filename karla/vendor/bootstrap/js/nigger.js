3e6467f2c975f5f32fe3fb03b292e471669c8fac
Afrasinei Serban <afrasinei.serban@gmail.com>
2019-11-24 14:22:14 +0200

    site-ul facut de croi + legat cu site-ul principal

---
+++ karla/vendor/bootstrap/js/nigger.js
@@ -0,0 +1,3 @@
+var nume = function(){
+    document.querySelector("#page-content-wrapper > div > div").innerHTML = '<label for="usr">Nume:</label><input type="text" class="form-control" id="usr"><br><button type="button" class="btn btn-success">Cauta</button>';
+}
